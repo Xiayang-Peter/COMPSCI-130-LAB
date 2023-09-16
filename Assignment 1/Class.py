b9a0de0dee140a231cc54ffcc63c972d08078ff7
Xiayang-Peter <xiayang123wert@Outlook.com>
2023-09-16 20:51:51 +1200

    130 Assignment 2 Question 3 complete  2023/9/16

--- Assignment 1/Class.py
+++ Assignment 1/Class.py
@@ -1,38 +1,91 @@
 class CardPile:
     def __init__(self):
         self.items = []
+
     def add_top(self, item):
+        #?# adds a new item to the top of the pile (at items[0]).
         self.items.insert(0,item)
+
     def add_bottom(self, item):
+        #?# This method adds a new item to the bottom of the pile (at items[self.size()-1]).
         self.items.append(item)
+
     def remove_top(self):
+        #?# This method removes and returns an item from the top of the pile (at items[0]).
         removed = self.items[0]
         self.items.pop(0)
         return removed
+    
     def remove_bottom(self):
+        #?# This method removes and returns an item from the bottom of the pile (at items[self.size()-1]).
         removed = self.items[-1]
         self.items.pop(-1)
         return removed
-    def size(self):
-        return(len(self.items))
-    def peek_top(self):
-        return(self.items[0])
-    def peek_bottom(self):
-        return(self.items[-1])
-    def print_all(self, index):
-        if index == 1:
-            output = ""
-            for numbers in self.items:
-                output += f"{numbers} "
-            print(output)
-        elif index == 0:
-            output = ""
-            output += f"{self.items[0]} "
-            for i in range(len(self.items) - 1):
-                output += "* "
-            print(output)
-               
+    
+    def clear(self):
+        self.items = []
 
+    def remove_all(self):
+        #!# CAREFUL THIS FUNCTION DELETE THE PILE AND RETURNS A WHOLE LIST 
+        removed_items = self.items
+        self.clear()
+        return removed_items
+
+    def is_empty(self):
+        return self.items == []
+
+
+    def size(self):
+        #?# This method returns the size of the pile.
+        return(len(self.items))
+    
+    def peek_top(self):
+        #?# This method returns the top card of the pile (at items[0]).
+        return(self.items[0])
+    
+    def peek_bottom(self):
+        #?# This method returns the bottom card of the pile (at items[self.size()-1]).
+        #!#return(self.items[self.size() - 1])
+        return(self.items[-1])
+
+    def print_all(self, index):               
+        if index >= 1:           
+        #?#  when index goes to 1, all numbers printed visible                                   
+            printed_pile = ""
+            for digits in self.items:
+                printed_pile += f"{digits} "
+            print(printed_pile)
+        elif index == 0:
+        #?#  when index goes to 1, all numbers printed visible   
+            printed_pile = ""
+            try:
+            #!# improved when pile contains no numbers
+                printed_pile += f"{self.items[0]} "
+                for i in range(self.size() - 1):
+                    printed_pile += "* "
+                print(printed_pile)
+            except IndexError:
+                print()          
+
+    def return_all(self, index):        
+        #?# improvement to the def print_all
+        if index >= 1:           
+        #?#  when index goes to 1, all numbers printed visible                                   
+            returned_pile = ""
+            for digits in self.items:
+                returned_pile += f"{digits} "
+            return returned_pile
+        elif index == 0:
+        #?#  when index goes to 1, all numbers printed visible   
+            returned_pile = ""
+            try:
+                returned_pile += f"{self.items[0]} "
+                for i in range(self.size() - 1):
+                    returned_pile += "* "
+                return returned_pile
+            except IndexError:
+                return ""
+            
         
 class Solitaire:
     def __init__(self, cards):
@@ -46,11 +99,35 @@ class Solitaire:
             self.piles[0].add_bottom(cards[i])
 
     def get_pile(self, i):
+        #?# returns out the specific line of number based on pile index i             
         return self.piles[i]
     
-"""    def display(self):
+    def display(self):
+        for i in range(self.num_piles):
+            print(f"{i}: {self.get_pile(i).return_all(i)}")
+
+    def move(self, p1 = 0, p2 = 0):
+        if p1 == p2 == 0:
+            for i in range(self.get_pile(p1).size() - 1):
+                #?# redeced one to stop after any futher move 
+                self.get_pile(p1).items[i], self.get_pile(p1).items[i + 1] = self.get_pile(p1).items[i + 1], self.get_pile(p1).items[i]
+        elif (p1 == 0 < p2) and not self.get_pile(p1).is_empty():
+            try:
+                if self.get_pile(p2).peek_bottom() - 1 == self.get_pile(p1).peek_top():
+                    self.get_pile(p2).add_bottom(self.get_pile(p1).remove_top())
+            except IndexError:
+                self.get_pile(p2).add_bottom(self.get_pile(p1).remove_top())
+        elif (p1 and p2) > 0 and not self.get_pile(p1).is_empty() and not self.get_pile(p2).is_empty():
+            if self.get_pile(p2).peek_bottom() - 1 == self.get_pile(p1).peek_top():
+                for p1_elements in (self.get_pile(p1).remove_all()):
+                    self.get_pile(p2).add_bottom(p1_elements)
+        else: #?# not vaild 
+            pass
+
+
+"""    
  
-    def move(self, p1, p2):
+    
 
     def is_complete(self):
 
--- Assignment 1/draft.py
+++ Assignment 1/draft.py
@@ -1,9 +1,21 @@
 from Class import CardPile
 from Class import Solitaire
 
-
-cards = [5, 13, 9, 6, 12, 8, 11, 14, 10, 7, 1, 2, 0, 3, 4]
+	
+	
+	
+	
+print("Testing Condition 3")
+cards = [6, 2, 5, 4, 1, 3]
 game = Solitaire(cards)
-game.get_pile(0).add_top(15)
-game.get_pile(0).print_all(0)
-game.get_pile(0).print_all(1)
+game.display()
+game.move(0, 2)
+game.move(0, 1)
+game.move(0, 2)
+game.move(0, 2)
+game.move(0, 1)
+print("AFTER 6 MOVES")
+game.display()
+game.move(1, 2)
+print("AFTER INVALID MOVE (value of moving card not one less than bottom of pile)")
+game.display()
